d556e0abf8b0047ef2c3d24fb6db88d158d87603
suzie6087 <41510235+suzie6087@users.noreply.github.com>
2020-11-08 22:55:11 +0900

    init commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">추가</button>
+    </div>
+</template>
+
+<script>
+export default {
+   methods:{
+       addTodo: function(){
+           console.log(this.newTodoItem);
+       }
+   }
+}
+</script>
+
+<style>
+</style>
